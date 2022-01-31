46108dfaaca78636e098521751ddba1a6f585039
Kyla Quilos <quilos.kyla@gmail.com>
2022-01-31 15:23:08 -0800

    updated charts

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,37 +1,48 @@
 import React, { useEffect, useState } from "react";
-import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Cell, Legend, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ events }) => {
 	const [data, setData] = useState([]);
-	useEffect(() => { setData(() => getData()); }, [events]);
+
+	useEffect(() => {
+		const getData = () => {
+			const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+			const data = genres.map(genre => {
+				const value = events.filter(event =>
+					event.summary.split(' ').includes(genre)
+				).length;
+				return { name: genre, value };
+			});
+			return data;
+		};
+		setData(() => getData());
+	}, [events]);
+
 	const colors = [ '#2DBBD9', '#F2DE25', '#37B928', '#24618C', '#E62913'  ];
 
-	const getData = () => {
-		const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-		const data = genres.map((genre) => {
-			const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
-			return { name: genre, value };
-		});
-		return data;
-	}
-
 	return (
-		<ResponsiveContainer width="100%" height="100%">
+		<ResponsiveContainer height={400}>
 			<PieChart width={400} height={400}>
 				<Pie
 					data={data}
-					cx="50%"
-					cy="50%"
+					cx='50%'
+					cy='50%'
 					labelLine={false}
-					outerRadius={80}
-					fill="#8884d8"
-					dataKey="value"
+					outerRadius={150}
+					fill='#8884d8'
+					dataKey='value'
 					label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
 				>
-					{data.map((index) => (
+					{data.map((entry, index) => (
 						<Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
 					))}
 				</Pie>
+				<Legend
+					layout='horizontal'
+					verticalAlign='top'
+					align='center'
+					height={45}
+				/>
 			</PieChart>
 		</ResponsiveContainer>
 	);
